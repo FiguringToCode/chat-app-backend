7ba93e43686852354abaec767e72aabeb49f89ef
FiguringToCode <marineryash2000@gmail.com>
2025-12-08 14:02:09 +0530

    Update index.js
    Removed deprecated mongoose connection options.

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ app.use(express.json())
 
 
 mongoose.connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB"))
   .catch(error => console.error("MongoDB connection error", error))
 
